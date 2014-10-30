cf5095169236efbb2aa6e28b5fbcb9224859322b
Adrian Puiu <blacksyforce@yahoo.com>
2014-10-30 17:26:07 +0200

    update dist

---
+++ dist/scripts/config.c43e8516.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'development',ApiUrl:'http://navigatorglassweb.cloudapp.net:80/api',oAuth:'http://6b74cc2ca3764a3db82c8499ef1795aa.cloudapp.net'})
+
+;
