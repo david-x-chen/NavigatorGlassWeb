f2c8243775d6dd1671bd11edb6b9f5729cbf621b
Brian Me <meduccuong@gmail.com>
2014-12-03 18:32:27 +0700

    #17
    Add back, next button
    Todo: Implement logic

---
+++ app/scripts/directives/card.js
@@ -0,0 +1,15 @@
+﻿angular.module('navigatorGlassProjectApp')
+.controller('CardCtrl',function($scope){    
+    
+
+
+    $scope.onPrev = function() {
+        console.log('prev');
+    };    
+
+    $scope.onNext = function() {
+        console.log('next');
+    };
+
+    
+});
