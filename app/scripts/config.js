6d15f5040ffdfbff09ab083844f06f772f81b182
Cristian Maghiar <cristi@avangard-media.ro>
2014-12-13 18:45:39 +0200

    updated .gitignore, added app.scss

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',ApiUrl:'http://navigatorglassweb.cloudapp.net:80/api',oAuth:'http://6b74cc2ca3764a3db82c8499ef1795aa.cloudapp.net'})
+.constant('ENV', {name:'production',ApiUrl:'http://navigatormock.cloudapp.net/api',oAuth:'http://6b74cc2ca3764a3db82c8499ef1795aa.cloudapp.net'})
 
 ;
